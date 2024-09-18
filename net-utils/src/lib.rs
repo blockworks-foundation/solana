0b10ea09b3357ed62685ac21a0c36fd21ddedafe
GroovieGermanikus <groovie@mango.markets>
2024-09-18 20:44:20 +0200

    simplify parser

--- net-utils/src/lib.rs
+++ net-utils/src/lib.rs
@@ -1,5 +1,8 @@
 //! The `net_utils` module assists with networking
 #![allow(clippy::arithmetic_side_effects)]
+
+use std::net::Ipv6Addr;
+use std::str::FromStr;
 use {
     crossbeam_channel::unbounded,
     log::*,
@@ -341,6 +344,12 @@ pub fn parse_port_range(port_range: &str) -> Option<PortRange> {
 }
 
 pub fn parse_host(host: &str) -> Result<IpAddr, String> {
+
+    println!("SKIP CHECK: HOST={}", host);
+    if true {
+        return Ok(SocketAddr::from_str(&format!("{}:0", host)).expect("valid IP").ip());
+    }
+
     // First, check if the host syntax is valid. This check is needed because addresses
     // such as `("localhost:1234", 0)` will resolve to IPs on some networks.
     let parsed_url = Url::parse(&format!("http://{host}")).map_err(|e| e.to_string())?;
